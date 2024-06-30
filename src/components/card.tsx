bbf78fb5db754621f7bc2569fb26312576ae81a5
harisxayyaz <harisayyaz42@gmail.com>
2024-06-30 11:34:33 +0500

    corrected button transitions

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -13,9 +13,8 @@ const Card: React.FC<CardProps> = ({ name, description }) => {
         {description}
       </p>
 
-      <button className="flex font-medium text-sm gap-1 items-center text-white animate-fadeIn justify-center hover:justify-between hover:pl-4 hover:pr-4 bg-green-700 w-36 h-10 rounded-xl hover:bg-green-900 hover:border-white hover:border-2 transition-all duration-300">
-        <p className="transform">View Papers</p>
-        <p className="transform ml-0 hover:ml-5"> -{">"}</p>
+      <button className="flex font-medium text-sm gap-1 items-center text-white justify-center hover:justify-between hover:pl-4 hover:pr-4 bg-green-700 w-36 h-10 rounded-xl hover:bg-green-900 hover:border-white hover:border-2 ease-out duration-300">
+        <p className="transform">View Papers -{">"}</p>
       </button>
     </div>
   );
