4d439fb0bdda460b16e6a369dc8af3922c95ead4
harisxayyaz <harisayyaz42@gmail.com>
2024-07-08 12:28:23 +0500

    added subjects

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -16,19 +16,24 @@ const Card: React.FC<CardProps> = ({ name, description, links }) => {
   };
 
   return (
-    <div
+    <main
       className="  flex flex-col p-4 bg-white border-2 h-72 w-72 text-2xl font-bold rounded-xl pt-8 pb-8 cursor-pointer transition-transform duration-300 hover:shadow-md transform hover:-translate-y-1"
       onClick={handleClick}
     >
-      <h1 className=" h-[70px]">{name}</h1>
-      <p className="text-base text-left text-gray-600 h-[90px]">
-        {description}
-      </p>
-
-      <button className="flex font-medium text-sm gap-1 items-center text-white justify-center bg-green-700 w-36 h-10 rounded-xl hover:bg-green-900 ease-out duration-200">
-        <p className="transform">View Papers -{">"}</p>
-      </button>
-    </div>
+      <div className="flex h-[90px]">
+        <h1>{name}</h1>
+      </div>
+      <div className="">
+        <p className="text-base text-left text-gray-600 h-[90px]">
+          {description}
+        </p>
+      </div>
+      <div className="">
+        <button className="flex font-medium text-sm gap-1 items-center text-white justify-center bg-green-700 w-36 h-10 rounded-xl hover:bg-green-900 ease-out duration-200">
+          <p className="transform">View Papers -{">"}</p>
+        </button>
+      </div>
+    </main>
   );
 };
 
